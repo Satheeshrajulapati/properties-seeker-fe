7d703ff9bc948dda6773d76968092abd02375b5c
ShivaKrishna2266 <gadudulashiva@gmail.com>
2025-09-04 00:27:26 +0530

    setup project folder structure (core, shared, auth, features)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,24 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { LoginComponent } from './components/login/login.component';
-import { LogoutComponent } from './components/logout/logout.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LogoutComponent } from './auth/logout/logout.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+import { HomeComponent } from './features/home/home.component';
+
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    // generic dashboard
     { path: 'dashboard', component: DashboardComponent },
-    { path: 'login', component: LoginComponent},
-    { path: 'register', component: RegisterComponent},
-    { path: 'logout', component: LogoutComponent},
+    { path: 'home', component:HomeComponent},
+
+    
+    // role-based dashboards
+
+    // auth routes
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'logout', component: LogoutComponent },
     { path: '**', component: PageNotFoundComponent }
 ];
